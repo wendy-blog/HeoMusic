1f04c0cabd8c4b68620a5bc1b2f6e397d534016d
文迪 <wendy8wendy@hotmail.com>
2024-11-19 21:08:37 +0800

    Update localEngine.js

--- js/localEngine.js
+++ js/localEngine.js
@@ -19,3 +19,18 @@ const ap = new APlayer({
   lrcType: 3,
   audio: encodedLocalMusic
 });
+  var localMusic = [{
+      name: '你瞒我瞒',
+      artist: '陈柏宇',
+      url: '../music/你瞒我瞒.mp3',
+      cover: './music/你瞒我瞒.png',
+      lrc: './music/你瞒我瞒.lrc'
+  },
+  {
+      name: '哭砂',
+      artist: '张杰',
+      url: '../music/张杰-哭砂.mp3',
+      cover: '../music/张杰-哭砂.png',
+      lrc: '../music/张杰-哭砂.lrc'
+  }
+  ];
